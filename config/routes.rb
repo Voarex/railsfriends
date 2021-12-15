c40fab090003b6bc83800fae5dd233af857a046c
Voarexxx <coltergellis@icloud.com>
2021-12-15 02:36:48 -0700

    adds messaging incomlete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :rooms do
+    resources :messages 
+  end
   devise_for :users
   resources :friends
   #get 'home/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_032043) do
+ActiveRecord::Schema.define(version: 2021_12_15_081858) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,20 @@ ActiveRecord::Schema.define(version: 2021_12_15_032043) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_friends_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "room_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +50,5 @@ ActiveRecord::Schema.define(version: 2021_12_15_032043) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
 end
